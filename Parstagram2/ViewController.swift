88789cf133f593b9e02da2c704bba800bcdc5e85
Ann Wu <weian.wu824@hotmail.com>
2022-03-26 18:29:38 -0600

    Initial Commit

---
+++ Parstagram2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram2
+//
+//  Created by mac book pro on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
